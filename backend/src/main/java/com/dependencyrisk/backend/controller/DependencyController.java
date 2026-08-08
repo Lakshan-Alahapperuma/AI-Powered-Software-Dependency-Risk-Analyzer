edb1d71256367eb2af8c7a21c9ffdccccc9c8e47
Lakshan Alahapperuma <lakshanalahapperuma@Lakshans-MacBook-Air.local>
2026-08-08 17:59:23 +0530

    add dependency extract info

--- backend/src/main/java/com/dependencyrisk/backend/controller/DependencyController.java
+++ backend/src/main/java/com/dependencyrisk/backend/controller/DependencyController.java
@@ -1,5 +1,6 @@
 package com.dependencyrisk.backend.controller;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
@@ -8,7 +9,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.dependencyrisk.backend.entity.Dependency;
 import com.dependencyrisk.backend.service.DependencyService;
@@ -19,7 +22,9 @@ public class DependencyController {
 
     private final DependencyService dependencyService;
 
-    public DependencyController(DependencyService dependencyService) {
+    public DependencyController(
+            DependencyService dependencyService) {
+
         this.dependencyService = dependencyService;
     }
 
@@ -29,7 +34,9 @@ public class DependencyController {
             @RequestBody Dependency dependency) {
 
         Dependency savedDependency =
-                dependencyService.addDependency(projectId, dependency);
+                dependencyService.addDependency(
+                        projectId,
+                        dependency);
 
         return ResponseEntity.ok(savedDependency);
     }
@@ -39,7 +46,22 @@ public class DependencyController {
             @PathVariable Long projectId) {
 
         return ResponseEntity.ok(
-                dependencyService.getDependenciesByProject(projectId)
+                dependencyService
+                        .getDependenciesByProject(projectId)
         );
+    }
+
+    @PostMapping("/upload")
+    public ResponseEntity<List<Dependency>> uploadPackageJson(
+            @PathVariable Long projectId,
+            @RequestParam("file") MultipartFile file)
+            throws IOException {
+
+        List<Dependency> dependencies =
+                dependencyService.uploadPackageJson(
+                        projectId,
+                        file);
+
+        return ResponseEntity.ok(dependencies);
     }
 }
--- backend/src/main/java/com/dependencyrisk/backend/service/DependencyService.java
+++ backend/src/main/java/com/dependencyrisk/backend/service/DependencyService.java
@@ -1,26 +1,38 @@
 package com.dependencyrisk.backend.service;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.dependencyrisk.backend.entity.Dependency;
 import com.dependencyrisk.backend.entity.Project;
 import com.dependencyrisk.backend.repository.DependencyRepository;
 import com.dependencyrisk.backend.repository.ProjectRepository;
 
+import tools.jackson.databind.JsonNode;
+import tools.jackson.databind.ObjectMapper;
+
+
+
 @Service
 public class DependencyService {
 
     private final DependencyRepository dependencyRepository;
     private final ProjectRepository projectRepository;
+    private final ObjectMapper objectMapper;
 
     public DependencyService(
             DependencyRepository dependencyRepository,
-            ProjectRepository projectRepository) {
+            ProjectRepository projectRepository,
+            ObjectMapper objectMapper) {
 
         this.dependencyRepository = dependencyRepository;
         this.projectRepository = projectRepository;
+        this.objectMapper = objectMapper;
     }
 
     public Dependency addDependency(Long projectId, Dependency dependency) {
@@ -35,7 +47,72 @@ public class DependencyService {
     }
 
     public List<Dependency> getDependenciesByProject(Long projectId) {
-
         return dependencyRepository.findByProjectId(projectId);
+    }
+
+    public List<Dependency> uploadPackageJson(
+            Long projectId,
+            MultipartFile file) throws IOException {
+
+        Project project = projectRepository.findById(projectId)
+                .orElseThrow(() ->
+                        new RuntimeException("Project not found with id: " + projectId));
+
+        JsonNode root = objectMapper.readTree(file.getInputStream());
+
+        List<Dependency> dependencies = new ArrayList<>();
+
+        JsonNode normalDependencies = root.get("dependencies");
+
+        if (normalDependencies != null && normalDependencies.isObject()) {
+            extractDependencies(
+                    normalDependencies,
+                    project,
+                    dependencies
+            );
+        }
+
+        JsonNode devDependencies = root.get("devDependencies");
+
+        if (devDependencies != null && devDependencies.isObject()) {
+            extractDependencies(
+                    devDependencies,
+                    project,
+                    dependencies
+            );
+        }
+
+        return dependencyRepository.saveAll(dependencies);
+    }
+
+    private void extractDependencies(
+            JsonNode dependencyNode,
+            Project project,
+            List<Dependency> dependencies) {
+
+        for (Map.Entry<String, JsonNode> entry : dependencyNode.properties()) {
+
+    String packageName = entry.getKey();
+    String version = entry.getValue().asString();
+
+    version = cleanVersion(version);
+
+    Dependency dependency =
+            new Dependency(packageName, version, project);
+
+    dependencies.add(dependency);
+}
+    }
+
+    private String cleanVersion(String version) {
+
+        if (version == null) {
+            return null;
+        }
+
+        return version
+                .replace("^", "")
+                .replace("~", "")
+                .trim();
     }
 }
